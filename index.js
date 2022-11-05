e29d747a1c8022c4dd9dea7c4a83efba61c5e8bc
Monika Gorka <monikagorka@gmail.com>
2022-11-05 15:23:20 +0100

    FUNCTION - EX. 2 - suma 2 liczb

--- index.js
+++ index.js
@@ -1,66 +1,11 @@
-// FUNCTION
-// kawałek kodu opakowany w nazwę, który możemy używać w różnych miejscach
-// funkcja zwraca wartość przy wywołaniu (używaniu) funkcji (choć nie wszystkie funkcji muszą coś zwracać np. funkcja push, console.log)
-// funkcja ma argument - parametr - wartość wejściowa - nie musi mieć
-
-// DEKLARACJA FUNKCJI
-// function nazwaFunkcji(arguemnty) {skrzynka z naszym kawałkiem kodu - tego nie widzimy, wiemy co może z tej fukcji wyskoczyć}
-
-// PRZYKŁAD
-// function myFunction () {
-//     let x = "Moje info";
-//     console.log(`To jest: ${x}`);
-// }
-
-// myFunction();
-// to jest wywołanie funkcji i dopiero zadziała
-
-
 // ***********************
-// PRZYKŁAD 2
+// FUNCTION - Ex. 2
 
-// function myFunction(myText) {
-//     console.log(`To jest: ${myText}`);
-// }
+function add(num1, num2) {
+    return num1 + num2;
+}
 
-// myFunction("Moj super text");
-
-// lub
-
-// let x ="Moj super text";
-// myFunction(x);
+console.log(add(1, 76));
 
 
-// ***********************
-// PRZYKŁAD 3 - return
-
-// function myFunction(myText) {
-//     return `To jest: ${myText}`;
-//     let x = 3;
-    // let x się nie wykona, bo wcześniej jest return
-// }
-
-// myFunction("My super text");
-// samo wywołanie nie spowoduje żadnego efektu. nie przypisaliśmy do zmiennej
-
-// console.log(myFunction("My super text"));
-// wywołanie funkcji, dopiero teraz się wyświetla
-
-
-// ***********************
-// PRZYKŁAD 4 - FUNKCJA ANONIMOWA
-
-// const mySecondFunction = function(myText) {
-//     return `To jest: ${myText}`;
-// };
-// przypisanie funkcji anonimowej
-
-
-// ***********************
-// PRZYKŁAD 5 - FUNKCJA STRZAŁKOWA
-
-const myThirdFunction = (myText) => {
-    return `To jest: ${myText}`;
-};
-// przypisanie funkcji strzałkowej
 
