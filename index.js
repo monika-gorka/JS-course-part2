2126c216faf30ad23fe081b3a902028c364462ad
Monika Gorka <monikagorka@gmail.com>
2022-11-05 12:21:49 +0100

    TABLE - Ex.4 - suma elementów w pętli

--- index.js
+++ index.js
@@ -8,3 +8,16 @@
 // }
 
 // console.log(tab);
+
+
+// ***********************************
+// TABLE - Ex. 4
+
+// const tab = [6, 4, 3, 7, 2, 1, 5, 8, 5, 3];
+
+// let sum = 0;
+// for(let i = 0; i <10; i++) {
+// sum = sum + tab[i];
+// }
+
+// console.log(sum);
