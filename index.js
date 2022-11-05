c02ee61bf23000440da723a241642ac410a1a1da
Monika Gorka <monikagorka@gmail.com>
2022-11-05 15:30:49 +0100

    FUNCTION - EX. 6 - suma elementów tablicy

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // ***********************
 // FUNCTION - Ex. 2
 
-function add(num1, num2) {
-    return num1 + num2;
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// console.log(add(1, 76));
+
+
+// ***********************
+// FUNCTION - Ex. 6
+
+function sumTab(tab) {
+    let sum = 0;
+
+    for (let i = 0; i < tab.length; i++) {
+        sum = sum + tab[i];
+    }
+    return sum;
 }
 
-console.log(add(1, 76));
-
-
-
+console.log(sumTab([6, 5, 4]));
