df8284c939782f54ff2b02bb94b31f2a30fe382b
Monika Gorka <monikagorka@gmail.com>
2022-11-05 15:45:22 +0100

    FUNCTION - EX. 7 - liczba parzysta

--- index.js
+++ index.js
@@ -22,13 +22,31 @@
 
 // console.log(sumTab([6, 5, 4]));
 
-const sumTab = (tab) => {
-        let sum = 0;
+// const sumTab = (tab) => {
+//         let sum = 0;
     
-        for (let i = 0; i < tab.length; i++) {
-            sum = sum + tab[i];
+//         for (let i = 0; i < tab.length; i++) {
+//             sum = sum + tab[i];
+//         }
+//         return sum;
+//     }
+    
+//     console.log(sumTab([6, 5, 4]));
+
+
+// ***********************
+// FUNCTION - Ex. 7
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function showEvenNumbers(tab) {
+    for(let i = 0; i < tab.length; i++) {
+        if(isEven(tab[i])) {
+            console.log(tab[i]);
         }
-        return sum;
     }
-    
-    console.log(sumTab([6, 5, 4]));
+}
+
+showEvenNumbers([3, 6, 4, 2, 56]);
