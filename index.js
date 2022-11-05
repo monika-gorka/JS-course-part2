776fd2cd94a8058ed445fc791122b291045f680d
Monika Gorka <monikagorka@gmail.com>
2022-11-05 12:39:50 +0100

    TABLE - Ex. 5 - pętla w pętli

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// **********************************
 // TABLE - Ex. 1
 
 // const tab = [];
@@ -21,3 +22,27 @@
 // }
 
 // console.log(sum);
+
+
+// ***********************************
+// TABLE - Ex. 5 PĘTLA w PĘTLI
+
+// const tab = [6, 4, 3, 7, 2, 1, 5, 8, 5, 3];
+
+// let duplicate = false;
+
+// for(let i = 0; i < tab.length; i++) {
+//     let toCheck = tab[i];
+//     for (let j = 0; j < tab.length; j++) {
+//         if(toCheck === tab[j] && i !==j) {
+//             duplicate = true;
+//         }
+//     }
+// }
+
+// if (duplicate) {
+//     console.log("Są duplikaty!")
+// }
+
+// i !== j - warunek, że indeksy liczb są różne - 3 jest 3 nierówna
+// zmienna duplicate - pomocnicza. Na początku false, bo zakładam, że nie wystąpiły
