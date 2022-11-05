288b0a175bccf9fa8d63e0ff3de53aa36af22167
Monika Gorka <monikagorka@gmail.com>
2022-11-05 11:34:11 +0100

    TABLE - basic information

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-// // Zadanie 4 - String
-// // let text = "12345667778886";
-// // console.log(text.slice(-6));
-// console.log(text.substring(text.lenght -6));
+// TABLICA
+// zmienna przechowująca wiele wartości
+// struktura programistyczna
+// ma nazwę, do której możemy się odwoływać
+// tablica zawiera elementy - "mniejsze pudełka z wartościami" z unikalnymi identyfikatorami - indeksami
+// indeksujemy - numerujemy kolejne wartości w tablicy, zaczynając od "0"
+// [] - wskazują, że jest to tablica
+// typy wartości powinny być jednolite
+
+// TABLICA
+// let nazwa_tablicy [element1, element2, ...]
+
+// PUSTA TABLICA
+// let tab = [];
+
+// TABLICA 5-ELEMENTOWA, NAJWYŻY INDEKS = 4, NAJWIĘKSZA WARTOŚĆ = 98
+// let tab = [1, 2, 4, 5, 98]
+
+// ODWOŁANIE DO KONKRETNEGO ELEMENTU W TABLICY - ZA POMOCĄ INDEKSU
+// console.log(tab[3]);
+// wyświetli się 5
+
+// ODWOŁANIE DO INDEKSU, KTÓRY NIE ISTNIEJE
+// wyświetli się undefined
