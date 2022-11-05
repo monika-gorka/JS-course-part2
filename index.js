a41e3c5b858953c1f50cd364e0971a31aa682ac9
Monika Gorka <monikagorka@gmail.com>
2022-11-05 16:07:29 +0100

    FUNCTION - EX. 14 - min, max, średnia wartość z tabeli

--- index.js
+++ index.js
@@ -37,16 +37,58 @@
 // ***********************
 // FUNCTION - Ex. 7
 
-function isEven(num) {
-    return num % 2 === 0;
-}
+// function isEven(num) {
+//     return num % 2 === 0;
+// }
 
-function showEvenNumbers(tab) {
+// function showEvenNumbers(tab) {
+//     for(let i = 0; i < tab.length; i++) {
+//         if(isEven(tab[i])) {
+//             console.log(tab[i]);
+//         }
+//     }
+// }
+
+// showEvenNumbers([3, 6, 4, 2, 56]);
+
+
+// ***********************
+// FUNCTION - Ex. 14
+
+function findMinValue (tab) {
+    let min = tab[0];
+
     for(let i = 0; i < tab.length; i++) {
-        if(isEven(tab[i])) {
-            console.log(tab[i]);
+        if(min > tab[i]) {
+            min = tab[i];
         }
     }
+    return min;
 }
 
-showEvenNumbers([3, 6, 4, 2, 56]);
+const findMaxValue = function(tab) {
+    let max= tab[0];
+
+    for(let i = 0; i < tab.length; i++) {
+        if(max < tab[i]) {
+            max = tab[i];
+        }
+    }
+    return max;
+}
+
+const getAverage = (tab) => {
+    let sum= 0;
+
+    for(let i = 0; i < tab.length; i++) {
+        sum = sum + tab[i];
+        }
+
+    return sum;
+}
+
+const tab = [5, 34, 2, 1, 7, 45, 4];
+
+console.log(findMinValue(tab));
+console.log(findMaxValue(tab));
+console.log(getAverage(tab));
